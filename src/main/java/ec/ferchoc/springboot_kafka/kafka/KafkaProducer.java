ff35480e4ff68a6eb7bd604051ae44e318c47001
Fernando <wilmercaiza@gmail.com>
2023-10-19 16:54:58 -0500

    Add kafka producer

---
+++ src/main/java/ec/ferchoc/springboot_kafka/kafka/KafkaProducer.java
@@ -0,0 +1,20 @@
+package ec.ferchoc.springboot_kafka.kafka;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        log.info("Message send: {}", message);
+        this.kafkaTemplate.send("myTopic", message);
+    }
+
+}
