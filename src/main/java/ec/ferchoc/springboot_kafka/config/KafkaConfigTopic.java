658ac0faddb664fa4a58b978dcf2554f0551fa87
Fernando <wilmercaiza@gmail.com>
2023-10-19 16:41:24 -0500

    Add config to create Topic with spring boot

---
+++ src/main/java/ec/ferchoc/springboot_kafka/config/KafkaConfigTopic.java
@@ -0,0 +1,16 @@
+package ec.ferchoc.springboot_kafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfigTopic {
+
+    @Bean
+    public NewTopic myTopic() {
+        return TopicBuilder.name("my-topic").build();
+    }
+
+}
